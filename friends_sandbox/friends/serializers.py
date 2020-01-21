454c29f874ad919e9027a81e4e691102961d6927
JuriKovtun <kovtun.yurko@gmail.com>
2020-01-21 16:05:11 +0200

    stitching code pieces

--- friends_sandbox/friends/serializers.py
+++ friends_sandbox/friends/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from friendship.models import FriendshipRequest
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,10 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class FriendshipRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendshipRequest
+        fields = ['id', 'from_user', 'to_user',
+                  'message', 'created', 'rejected', 'viewed']
--- friends_sandbox/urls.py
+++ friends_sandbox/urls.py
@@ -21,6 +21,8 @@ from friends_sandbox.friends import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'friends', views.FriendViewSet)
+router.register(r'friendrequests', views.FriendshipRequestViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
